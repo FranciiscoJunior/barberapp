3b888bcb97cc0ac672e7e0cb42e4e17e3338bddd
Francisco Júnior <franciscoamj1001@gmail.com>
2025-10-07 14:40:31 -0300

    faet: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-import { Button } from "flowbite-react"
 import { SearchIcon } from "lucide-react"
-import { Input } from "./_components/ui/input"
 import Header from "./_components/header"
+import { Button } from "flowbite-react"
+import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar } from "./_components/ui/avatar"
+import { AvatarImage } from "./_components/ui/avatar"
 
 //SERVER COMPONENTS
 const Home = () => {
@@ -24,6 +28,30 @@ const Home = () => {
         <div className="relative mt-6 w-full h-[150px]">
           <Image alt="Agende nos melhores com FSW Barber" src="/banner-01.png" fill className="object-cover rounded-xl" />
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/*ESQUERDA*/}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"/>
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            {/*DIREITA*/}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Outubro</p>
+              <p className="text-2xl">07</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
