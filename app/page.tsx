dbf52cba8c826fafde3a85c195f0eeb7f46d3879
Francisco Júnior <franciscoamj1001@gmail.com>
2025-10-23 15:14:09 -0300

    feat: ensure bookings are listed on bookings page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Header from "./_components/header"
 import { Button } from "flowbite-react"
 import Image from "next/image"
-import BarbershopItem from "./_components/barbershoping-item"
 import { db } from "./_lib/prisma"
+import BarbershopItem from "./_components/barbershoping-item"
 import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
 import Search from "./_components/search"
 import Link from "next/link"
+import { getServerSession } from "next-auth"
+import { authOptions } from "./_lib/auth"
 
 //SERVER COMPONENTS
 const Home = async () => {
+  const session = await getServerSession(authOptions)
   const barbershops = await db.barbershop.findMany({})
   const popularBarbershops = await db.barbershop.findMany({
     orderBy: {
       name: "desc",
     },
   })
+
+  const confirmedBookings = session?.user
+    ? await db.booking.findMany({
+        where: {
+          userId: (session?.user as any).id,
+          date: {
+            gte: new Date(),
+          },
+        },
+
+        include: {
+          service: {
+            include: {
+              barbershop: true,
+            },
+          },
+        },
+        orderBy: {
+          date: "asc",
+        },
+      })
+    : []
 
   return (
     <div>
@@ -56,9 +83,16 @@ const Home = async () => {
           />
         </div>
 
-        {/*AGENDAMENTOS*/}
+        <h2 className="mt-6 mb-3 text-xs font-bold text-gray-400 uppercase">
+          Agendamentos
+        </h2>
 
-        {/*< BookingItem />*/}
+        {/*AGENDAMENTOS*/}
+        <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {confirmedBookings.bookings.map((booking) => {
+            ;<BookingItem key={booking.id} booking={booking} />
+          })}
+        </div>
 
         <h2 className="mt-6 mb-3 text-xs font-bold text-gray-400 uppercase">
           Recomendados
